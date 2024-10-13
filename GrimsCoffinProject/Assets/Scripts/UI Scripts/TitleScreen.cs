18cebe98a96b1cf9d1ec6f92f4a0889305e8a89f
Kevin-Insinna <kji8379@rit.edu>
2024-10-13 15:23:52 -0400

    Control Screen set up with Xbox diagram

--- GrimsCoffinProject/Assets/Scripts/UI Scripts/TitleScreen.cs
+++ GrimsCoffinProject/Assets/Scripts/UI Scripts/TitleScreen.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject controlsScreen;
+    [SerializeField] private GameObject start;
+    [SerializeField] private GameObject back;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +31,18 @@ public class TitleScreen : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ToggleControls()
+    {
+        controlsScreen.SetActive(!controlsScreen.activeInHierarchy);
+        this.gameObject.SetActive(!this.gameObject.activeInHierarchy);
+
+        if (controlsScreen.activeInHierarchy)
+            EventSystem.current.SetSelectedGameObject(back);
+
+        else
+            EventSystem.current.SetSelectedGameObject(start);
+
     }
 }
