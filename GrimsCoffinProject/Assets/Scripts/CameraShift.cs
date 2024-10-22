3dd73c6e8c3924e9e7f6f67bcb15341137c11cfc
npf9700 <70346282+npf9700@users.noreply.github.com>
2024-10-21 23:18:34 -0400

    Fixed bug and camera issues
    Fixed scaling of levels and camera shift bug

--- GrimsCoffinProject/Assets/Scripts/CameraShift.cs
+++ GrimsCoffinProject/Assets/Scripts/CameraShift.cs
@@ -29,7 +29,7 @@ public class CameraShift : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
