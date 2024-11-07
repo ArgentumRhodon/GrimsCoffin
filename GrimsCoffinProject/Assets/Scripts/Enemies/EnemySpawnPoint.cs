933eb1f5eaceafb4dc925b53653b47ad5fc97136
Kevin-Insinna <kji8379@rit.edu>
2024-11-07 15:55:17 -0500

    Added comments to enemy spawn scripts

--- GrimsCoffinProject/Assets/Scripts/Enemies/EnemySpawnPoint.cs
+++ GrimsCoffinProject/Assets/Scripts/Enemies/EnemySpawnPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemySpawnPoint : MonoBehaviour
 {
+    //Enemy to spawn at this location
     [SerializeField] private Enemy enemyToSpawn;
 
     void Start()
@@ -11,8 +12,14 @@ public class EnemySpawnPoint : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Spawn the enemy within the associated room
+    /// </summary>
+    /// <param name="room">The room to spawn the enemy in, used by the EnemyManager script</param>
+    /// <returns></returns>
     public Enemy SpawnEnemy(GameObject room)
     {
+        //Instatiates the enemy at the position of the spawn point with the room as the enemy's parent
         Instantiate(enemyToSpawn.gameObject, this.transform.position, Quaternion.identity, room.transform);
         return enemyToSpawn;
     }
