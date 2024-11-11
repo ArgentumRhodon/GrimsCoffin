99431f827636a8ad05922b953cc18c9cfda670d6
BoxiFeng9384 <bf9384@g.rit.edu>
2024-11-11 00:46:44 -0500

    Add Camera Border and Camera Shake

--- GrimsCoffinProject/Assets/Scripts/SceneTransition/Room.cs
+++ GrimsCoffinProject/Assets/Scripts/SceneTransition/Room.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Room : MonoBehaviour
 {
     [SerializeField]
     private bool hasPlayer;
+    private bool RoomLive;
+    [SerializeField] private GameObject Border;
+    [SerializeField] private CinemachineVirtualCamera VCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,17 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasPlayer)
+            Changeborder(Border);
+    }
+
+    private void Changeborder(GameObject border)
+    {
+        if (!RoomLive) 
+        {
+            CinemachineConfiner VConfiner = VCamera.GetComponent<CinemachineConfiner>();
+            VConfiner.m_BoundingShape2D = border.GetComponent<PolygonCollider2D>();
+            RoomLive = true;
+        }
     }
 }
