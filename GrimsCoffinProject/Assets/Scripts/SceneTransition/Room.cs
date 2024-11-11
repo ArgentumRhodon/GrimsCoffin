a1fec475352a596a108c85b498d5b5b46123335d
BoxiFeng9384 <bf9384@g.rit.edu>
2024-11-11 01:03:05 -0500

    Add Camera Transition

--- GrimsCoffinProject/Assets/Scripts/SceneTransition/Room.cs
+++ GrimsCoffinProject/Assets/Scripts/SceneTransition/Room.cs
@@ -21,7 +21,7 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (hasPlayer)
+        if (this.gameObject.activeSelf)
             Changeborder(Border);
     }
 
