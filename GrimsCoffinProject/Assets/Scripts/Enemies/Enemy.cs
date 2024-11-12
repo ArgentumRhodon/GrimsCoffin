4ce389d97aca723542bf663a8f4ff1f57700a8db
Kevin-Insinna <kji8379@rit.edu>
2024-11-12 15:06:44 -0500

    D-Pad Implementation & Camera Shake on attacking reduced

--- GrimsCoffinProject/Assets/Scripts/Enemies/Enemy.cs
+++ GrimsCoffinProject/Assets/Scripts/Enemies/Enemy.cs
@@ -47,7 +47,7 @@ public abstract class Enemy : MonoBehaviour
     public virtual void TakeDamage(float damage = 1)
     {
         health -= damage;
-        CameraShake.Instance.ShakeCamera(4, 3, .2f);
+        CameraShake.Instance.ShakeCamera(2.5f, 1.75f, .2f);
 
         if (health <= 0)
             DestroyEnemy();
