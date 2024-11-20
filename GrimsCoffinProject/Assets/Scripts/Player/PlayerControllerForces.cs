9a127952d931f357f412fef56233a9b177afe49a
Kevin-Insinna <70411851+Kevin-Insinna@users.noreply.github.com>
2024-11-20 13:18:13 -0500

    Adjusted Jump Cancel Gravity Mult Logic

--- GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
+++ GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
@@ -30,7 +30,7 @@ public class PlayerControllerForces : MonoBehaviour
     private bool isJumpCancel;
     private bool isJumpFalling;
     private int airJumpCounter = 0;
-    float jumpClamp;
+    private float jumpVelocity;
 
     //Wall Jump
     private float wallJumpStartTime;
@@ -247,7 +247,6 @@ public class PlayerControllerForces : MonoBehaviour
         //Key Down, continue jumping
         if (value.isPressed)
         {
-            jumpClamp = LastJumpTime;
             LastPressedJumpTime = Data.jumpInputBufferTime;
             LastJumpTime = 0;
         }
@@ -477,6 +476,7 @@ public class PlayerControllerForces : MonoBehaviour
         force = OffsetForce(force);
 
         rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        jumpVelocity = rb.velocity.y;
     }
 
     //Wall jump
@@ -736,7 +736,11 @@ public class PlayerControllerForces : MonoBehaviour
             else if (isJumpCancel)
             {
                 //Higher gravity if jump button released
-                SetGravityScale(Data.gravityScale * Data.jumpCancelGravityMult);
+                float jumpRatio = rb.velocity.y / jumpVelocity;
+                float jumpCancelRatio = Data.jumpCancelGravityMult * jumpRatio;
+                float jumpCancelScale = Mathf.Clamp(Data.jumpCancelGravityMult * jumpCancelRatio, Data.fastFallGravityMult, Data.jumpCancelGravityMult);
+                Debug.Log(jumpCancelScale);
+                SetGravityScale(Data.gravityScale * jumpCancelScale);
                 rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFallSpeed));
             }
             else if ((playerState.IsJumping || playerState.IsWallJumping || isJumpFalling) && Mathf.Abs(rb.velocity.y) < Data.jumpHangTimeThreshold)
