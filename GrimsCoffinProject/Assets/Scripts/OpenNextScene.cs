da8646003b830040390b37e36a7d8b2b7f25b49e
BoxiFeng9384 <bf9384@g.rit.edu>
2024-12-04 16:10:57 -0500

    Add CutScene

--- GrimsCoffinProject/Assets/Scripts/OpenNextScene.cs
+++ GrimsCoffinProject/Assets/Scripts/OpenNextScene.cs
@@ -1,28 +1,18 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class SceneController : MonoBehaviour
 {
-    // This will allow you to assign the next scene in the editor using its name
     [SerializeField] private string nextSceneName;
 
-    // Function to load the next scene
+    void OnEnable()
+    {
+        LoadNextScene();
+    }
     public void LoadNextScene()
     {
-        if (!string.IsNullOrEmpty(nextSceneName))
-        {
-            StartCoroutine(WaitAndLoad(0.45f));
-        }
-        else
-        {
-            Debug.LogError("Next scene name is not set!");
-        }
-    }
-
-    IEnumerator WaitAndLoad(float Delay)
-    {
-        yield return new WaitForSeconds(Delay);
         SceneManager.LoadScene(nextSceneName);
     }
 }
