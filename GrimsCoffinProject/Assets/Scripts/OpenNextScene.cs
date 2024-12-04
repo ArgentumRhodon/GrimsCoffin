5edba9e6bd416d97e6efdb76b47e18ff45a9d6a1
BoxiFeng9384 <bf9384@g.rit.edu>
2024-12-04 16:17:58 -0500

    Add a test object to test the cutscene

--- GrimsCoffinProject/Assets/Scripts/OpenNextScene.cs
+++ GrimsCoffinProject/Assets/Scripts/OpenNextScene.cs
@@ -7,10 +7,6 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private string nextSceneName;
 
-    void OnEnable()
-    {
-        LoadNextScene();
-    }
     public void LoadNextScene()
     {
         SceneManager.LoadScene(nextSceneName);
