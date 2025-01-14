b58d84dbf3696d3193d1f84d4192c9df12bf3614
Kevin-Insinna <70411851+Kevin-Insinna@users.noreply.github.com>
2025-01-14 15:35:39 -0500

    Added functions for existing PlayerPrefs

--- GrimsCoffinProject/Assets/Scripts/PersistentDataManager.cs
+++ GrimsCoffinProject/Assets/Scripts/PersistentDataManager.cs
@@ -6,6 +6,8 @@ public class PersistentDataManager : MonoBehaviour
 {
     public static PersistentDataManager Instance { get; private set; }
 
+    List<Spirit> spirits;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -23,12 +25,40 @@ public class PersistentDataManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public bool SpiritCollected(Spirit spirit)
+    {
+        //Debug.Log(spiritToSpawn.spiritID.ToString());
+        if (PlayerPrefs.GetString(spirit.spiritID.ToString()) != "Uncollected")
+        {
+            Debug.Log("Spirit Collected");
+            return true;
+        }
+
+        else
+            return false;
+    }
+    public void UpdateSpiritState(Spirit spirit)
+    {
+        if (spirit.spiritState != Spirit.SpiritState.Idle)
+        {
+            spirit.spiritState++;
+        }
+
+        PlayerPrefs.SetString(spirit.spiritID.ToString(), spirit.spiritState.ToString());
+    }
+
+    public void SetSpawnLocation()
+    {
+
     }
 }
