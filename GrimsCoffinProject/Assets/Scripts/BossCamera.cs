2298c1d4abc0a59af54662fb2580ad32a938b2dc
npf9700 <70346282+npf9700@users.noreply.github.com>
2025-03-05 14:30:47 -0500

    Denial Touchups
    Slight platform changes for QoL, as well as some enemy adjustments to get rid of NullRef Errors

---
+++ GrimsCoffinProject/Assets/Scripts/BossCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BossCamera : MonoBehaviour
+{
+    [SerializeField]
+    private CameraManager camMgr;
+
+    [SerializeField]
+    private CinemachineVirtualCamera bossVirtualCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            camMgr.ChangeCamera(bossVirtualCam);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            camMgr.CameraReset();
+        }
+    }
+}
