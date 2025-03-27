c6579655086232fea59d9abe032c581bfb7db7b8
Sebward <70398495+Sebward@users.noreply.github.com>
2025-03-27 14:44:58 -0400

    Better Hitstop and reworked enemies

--- GrimsCoffinProject/Assets/Scripts/Combat/Air Down Attack/AirDownState.cs
+++ GrimsCoffinProject/Assets/Scripts/Combat/Air Down Attack/AirDownState.cs
@@ -52,7 +52,7 @@ public class AirDownState : MeleeBaseState
     protected override void RegisterAttack(Collider2D collidersToDamage)
     {
         Vector2 knockbackForce = KnockbackForce(collidersToDamage.gameObject.GetComponent<Enemy>().transform.position);
-        collidersToDamage.gameObject.GetComponent<Enemy>().TakeDamage(knockbackForce, attackDamage, true, .5f);
+        collidersToDamage.gameObject.GetComponent<Enemy>().TakeDamage(knockbackForce, attackDamage, true, .5f, .15f);
         collidersDamaged.Add(collidersToDamage);
     }
 }
