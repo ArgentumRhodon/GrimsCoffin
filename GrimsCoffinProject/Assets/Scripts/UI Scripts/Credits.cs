6f537df6e41cd919661297c39db7815916b4da2f
Kevin-Insinna <70411851+Kevin-Insinna@users.noreply.github.com>
2025-04-08 14:00:49 -0400

    Fixed some minor bugs

--- GrimsCoffinProject/Assets/Scripts/UI Scripts/Credits.cs
+++ GrimsCoffinProject/Assets/Scripts/UI Scripts/Credits.cs
@@ -8,7 +8,7 @@ public class Credits : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
