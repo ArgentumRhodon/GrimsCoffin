4d09abff3abac71c1c20881fd931d2bcd050f464
Kevin-Insinna <70411851+Kevin-Insinna@users.noreply.github.com>
2025-04-08 11:55:33 -0400

    Adjusted Credits and fixed boss map icon not disappearing when defeating boss

--- GrimsCoffinProject/Assets/Scripts/UI Scripts/Credits.cs
+++ GrimsCoffinProject/Assets/Scripts/UI Scripts/Credits.cs
@@ -8,7 +8,7 @@ public class Credits : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = transform.position;
+
     }
 
     // Update is called once per frame
@@ -18,10 +18,11 @@ public class Credits : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + .05f, transform.position.z);
         }
+    }
 
-        else
-        {
-            this.transform.position = startingPosition;
-        }
+    public void ResetCredits()
+    {
+        transform.position = startingPosition;
+        this.gameObject.SetActive(false);
     }
 }
