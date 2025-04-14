c2acb96b8af8ff8d9eda712a0cf2f31a2d7f88a7
Ladymint21 <Shangshushi1999@gmail.com>
2025-04-14 15:58:59 -0400

    Update: Throwing Scythe

--- GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
+++ GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
@@ -140,6 +140,10 @@ public class PlayerControllerForces : MonoBehaviour
     [SerializeField] public EventReference damageSFX;
     protected EventInstance damageInstance;
 
+    [Tooltip("FMOD events for the taking damage")]
+    [SerializeField] public EventReference throwSFX;
+    public EventInstance throwInstance;
+
     [Header("Player VFX Reference")]
     [SerializeField] private PlayerVFX playervfx;
     [SerializeField] private Material defaultShader;
@@ -189,8 +193,7 @@ public class PlayerControllerForces : MonoBehaviour
         slideInstance = RuntimeManager.CreateInstance(slidingSFX);
         wallLeapInstance = RuntimeManager.CreateInstance(wallLeapSFX);
         damageInstance = RuntimeManager.CreateInstance(damageSFX);
-
-
+        throwInstance = RuntimeManager.CreateInstance(throwSFX);
     }
 
     //Methods to make player controls work and to access it in the code
