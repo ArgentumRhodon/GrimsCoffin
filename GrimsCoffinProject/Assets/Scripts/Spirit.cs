9a121e53b446a1ed8dbdeda0874f3446f631cc4d
Kevin-Insinna <70411851+Kevin-Insinna@users.noreply.github.com>
2025-04-16 16:03:58 -0400

    Added exclamation marks to onboarding spirits and minor dialogue adjustments

--- GrimsCoffinProject/Assets/Scripts/Spirit.cs
+++ GrimsCoffinProject/Assets/Scripts/Spirit.cs
@@ -92,6 +92,9 @@ public class Spirit : Interactable
         else if (spiritState == SpiritState.Unlocked && PersistentDataManager.Instance.EnemyCurrency >= upgradeCost)
             exclamationMark.SetActive(true);
 
+        else if (spiritID == SpiritID.Onboarding1 || spiritID == SpiritID.Onboarding2 || spiritID == SpiritID.Onboarding3)
+            exclamationMark.SetActive(true);
+
         else
             exclamationMark.SetActive(false);
     }
