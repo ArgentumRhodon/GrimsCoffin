5294828d28bd8d4fe2fd580db31bb65a3730feac
Sebward <70398495+Sebward@users.noreply.github.com>
2025-04-22 15:20:12 -0400

    Re-commented the temp data method

--- GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
+++ GrimsCoffinProject/Assets/Scripts/Player/PlayerControllerForces.cs
@@ -249,7 +249,7 @@ public class PlayerControllerForces : MonoBehaviour
         }
 
         PlayerAnimationManager.Instance.ChangeAnimationState(PlayerAnimationStates.Idle);
-        TempResetData();
+        //TempResetData();
     }
 
     private void Update()
